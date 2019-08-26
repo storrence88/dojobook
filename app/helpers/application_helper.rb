ec159b4d289abb91ba3a68e3307f1a7c5643c091
Steven Torrence <storrence88@gmail.com>
2019-08-25 20:55:52 -0500

    Set up devise sessions and registrations view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_class(key)
+    sym = key.to_sym
+    case sym
+    when :notice then "alert alert-info"
+    when :success then "alert alert-success"
+    when :error then "alert alert-error"
+    when :alert then "alert alert-error"
+    end
+  end
 end
