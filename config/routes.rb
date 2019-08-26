8ca0608330ec68ec6f35c6596e61c5b243705b68
Steven Torrence <storrence88@gmail.com>
2019-08-25 21:11:58 -0500

    Update registrations controller and update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   root to: 'users#index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }, path: '',
+    path_names: { sign_in: 'login', sign_up: 'signup' }
   resources :users
 end
