8ee7632f91f70768202052ba6daf40823e8e882b
Steven Torrence <storrence88@gmail.com>
2019-08-30 23:00:26 -0500

    Create route for professional profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   root to: 'users#index'
+  get 'professional_profile', to: 'users#profile', as: :profile
   devise_for :users, controllers: { registrations: 'registrations' }, path: '',
     path_names: { sign_in: 'login', sign_up: 'signup' }
   resources :users
