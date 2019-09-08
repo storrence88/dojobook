c1fc0975fb1909d32e2a8dffc1e05d066272e747
Steven Torrence <storrence88@gmail.com>
2019-09-08 18:12:54 -0500

    Add actions that help with users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,34 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, except: :index
 
   def index
-    @users = User.where()
+    @users = User.all
+  end
+
+  def new
+    current_user.friend_request(@user)
+    # redirect_back(fallback_location: root_path)
+    current_user.reload
+  end
+
+  def create
+    current_user.accept_request(@user)
+    # redirect_back(fallback_location: root_path)
+    current_user.reload
+  end
+
+  def destroy
+    current_user.reject_request(@user)
+    # redirect_back(fallback_location: root_path)
+    current_user.reload
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
