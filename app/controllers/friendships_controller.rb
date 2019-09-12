ccce5c9d77a8e2a53f112549b1b9611674b9ce46
Steven Torrence <storrence88@gmail.com>
2019-09-11 20:14:59 -0500

    Create friendship controller and actions

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
 class FriendshipsController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :set_user
+
+  def new
+    current_user.friend_request(@user)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def create
+    current_user.accept_request(@user)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    current_user.reject_request(@user)
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
